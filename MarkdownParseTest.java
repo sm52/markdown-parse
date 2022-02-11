03e1fe7c442a56f34d2940295a2e0d2ccfe859f4
sm52 <sakina.muckadam@gmail.com>
2022-02-10 19:13:24 -0800

    new bash file

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,13 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.ArrayList;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
